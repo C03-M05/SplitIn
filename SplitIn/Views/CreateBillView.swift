31d97f6c9097b631bb1e9c1454a4fa3e50b09759
mirandautami3 <mirandautami3@gmail.com>
2026-07-14 08:59:57 +0700

    feat/membuat file CreateBillView

---
+++ SplitIn/Views/CreateBillView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateBillView.swift
+//  SplitIn
+//
+//  Created by Miranda Utami on 14/07/26.
+//
+
+import SwiftUI
+
+struct CreateBillView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateBillView()
+}
