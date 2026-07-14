74b55a533aa4687e664650bf8f8d156fdce1652d
mirandautami3 <mirandautami3@gmail.com>
2026-07-14 10:43:58 +0700

    Feat/ membuat komponen navigation bar

--- SplitIn/Views/CreateBillView.swift
+++ SplitIn/Views/CreateBillView.swift
@@ -7,12 +7,3 @@
 
 import SwiftUI
 
-struct CreateBillView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-#Preview {
-    CreateBillView()
-}
